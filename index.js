13c57b53ec810b7a44e5020d92540bee12270861
nick <nicholas.z.papa@gmail.com>
2014-12-29 00:31:16 -0800

    added some style and kinda scrolls to the bottom but not really

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.get('/', function(request, response) {
 });
 
 var id;
+var num_mess = 0;
 
 io.on('connection', function(socket){
+
+
+	//#212346
 
 	//TODO: SET UP FUNCTION FILE FOR RGB AND SUCH
 	var id = Math.floor((Math.random() * 10) + 1);
@@ -17,16 +21,20 @@ io.on('connection', function(socket){
 	var g = Math.floor((Math.random() * 255) + 1);
 	var b = Math.floor((Math.random() * 255) + 1);
 
-	io.emit('chat message', '<p style="color:rgb(' + r + ',' + g + ',' + b + ');">' + id + ' has connected</p>');
+	//on connection
+	io.emit('chat message', '<p style="width:100%;margin-top:0;margin-bottom:0; padding: 3px; background-color:#212346; color:rgb(' + r + ',' + g + ',' + b + ');">' + id + ' has connected</p>');
+	console.log(id + ' says has connected');
+	io.emit('message sent');
 
   	socket.on('chat message', function(msg){
-	  	io.emit('chat message', '<p style="color:rgb(' + r + ',' + g + ',' + b + ');">' + id + ' : '  + msg +  '</p>');
-	    console.log(id + ' says ' + msg);
+	  	io.emit('chat message', '<p style="width:100%;margin-top:0;margin-bottom:0; padding: 3px; left:0px;background-color:#212346; color:rgb(' + r + ',' + g + ',' + b + ');"> ' + id + ' : '  + msg +  '</p>');
+	    console.log("Message " + ++num_mess + ": " + id + ' says ' + "\"" + msg  + "\"");
+	    io.emit('message sent');	    
   	});	
 
 	socket.on('disconnect', function(){
 		console.log(id + ' has disconnected');
-		io.emit('chat message', '<p style="color:rgb(' + r + ',' + g + ',' + b + ');">' + id + ' has disconnected</p>');
+		io.emit('chat message', '<p style="width:100%;margin-top:0;margin-bottom:0; padding: 3px; background-color:#212346; color:rgb(' + r + ',' + g + ',' + b + ');">' + id + ' has disconnected</p>');
 	});
 });
 
