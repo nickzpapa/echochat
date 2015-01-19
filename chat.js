61906b4d60c329256bbe66f6290c89ec8319ae0a
nick <nick@deb-desktop.attlocal.net>
2015-01-18 20:00:17 -0800

    Init commit

--- chat.js
+++ chat.js
@@ -1,63 +1,130 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var userDef;
-var id = null;
-var num_mess = 0;
-var user = null;
+
+// setting up express with sessions
+var connect = require('connect')
+	, express = require('express')
+	, app = express();
+
+var CookieParser = require('cookie-parser')
+	, cookieParser = CookieParser()
+	, expresssession = require('express-session');
+
+app.use(cookieParser);
+app.use(expresssession({
+	secret: '245fdsfg2rf43f43',
+	resave: false,
+	saveUninitialized: true,
+}));
+
+// setting up server with express
+var http = require('http')
+	, server = http.createServer(app);
+
+// setting up sockets
+var io = require('socket.io').listen(server);
+
+
+//chat data
+var msgcount = 0;
+var users = {};
+var names = [""];
+
+function getId(s) {
+	return String(s.handshake["headers"].cookie).split(';')[1].substring(17).split('.')[0];
+}
+
 
 app.get('/', function (request, response) {
-	response.sendFile(__dirname + '/index.html');
-});
+	var id = request.sessionID;
+	console.log("- -- -- -- - -- -- -- -- - -- -- - -- -- -- -- - -- -- - -- - ");
+	console.log("getting index:" + request.sessionID);	
 
-app.get('/chat', function (request, response) {
-	if (user == null || user.username==null || user.font==null || user.backgr==null){
+	var user = {
+		username	: "" ,
+		font 		: "" ,
+		backgr 		: ""
+	}
+
+	if(users[id] != null) {
+		response.write('There was an error D: Please refresh page.');
+	}
+	else {
+		users[id] = user;
+		console.log(users[id]);
 		response.sendFile(__dirname + '/index.html');
 	}
+});
+
+
+app.get('/chat', function (request, response) {
+
+	var id = request.sessionID;
+	var user = users[id];
+	console.log("- -- -- -- - -- -- -- -- - -- -- - -- -- -- -- - -- -- - -- - ");
+	if (user == null || user.username==null || user.font==null || user.backgr==null){
+		console.log(id + " could not get chat due to value nullity.");
+		response.sendFile(__dirname + '/index.html');
+		return true;
+	}
 	else{
-		console.log('user getting chat.html');
+		console.log(user.username + "(" + id + ") is getting chat.");
 		response.sendFile(__dirname + '/chat.html');
 	}
 });
 
-var chat = io.of('/chat');
+
+
 var userspace = io.of('/newuser');
 userspace.on('connection', function (socket) {
-	console.log("user has connected to newuser");
+	var id = getId(socket);
+	console.log("- -- -- -- - -- -- -- -- - -- -- - -- -- -- -- - -- -- - -- - ");
+	console.log(id + " has connected to newuser.");
+
 	socket.on('new user', function (msg) {
-		user = JSON.parse(msg);
+	console.log("- -- -- -- - -- -- -- -- - -- -- - -- -- -- -- - -- -- - -- - ");
+		
+		var user = JSON.parse(msg);
+		users[id] = user;
+		console.log(id + " has requested a new user: " + user.username);
+
 	});
 	userspace.on('disconnect', function (msg) {
-		console.log("user has disconnected from newuser");
+		console.log( id + " has disconnected from newuser");
 	});
 });
 
 
+var chat = io.of('/chat');
 chat.on('connection', function (socket){
-	id = 0;
+	var id = getId(socket);
+	var user = users[id];
+
 	if (user == null || user.username==null || user.font==null || user.backgr==null){
-		chat.emit('chat message', 'you have no username');
+		chat.emit('whoops', 'You have no username.');
 		return false;
 	}
 
 	//on connection
 	chat.emit('chat message', '<p style="width:100%;margin-top:0;margin-bottom:0; padding: 3px; background-color:' + user.backgr + '; color:' + user.font + ';">' + user.username + ' has connected</p>');
-	console.log(user.username + ' says has connected');
+	console.log(user.username + '(' + id +' ) has connected to chat');
 	chat.emit('message sent');
 
+	//on message
   	socket.on('chat message', function (msg){
 	  	chat.emit('chat message', '<p style="width:100%;margin-top:0;margin-bottom:0; padding: 3px; background-color:' + user.backgr + '; color:' + user.font + ';">' + user.username + ' : '  + msg +  '</p>');
-	    console.log("Message " + ++num_mess + ": " + user.username + ' says ' + "\"" + msg  + "\"");
+	    console.log( "Message " + (++msgcount) +   ": " +   user.username +   ' says ' +   "\"" + msg  + "\"");
 	    chat.emit('message sent');	    
   	});	
 
+	//on disconnect
 	socket.on('disconnect', function(){
-		console.log(id + ' has disconnected');
+		console.log(user.username + ' has disconnected');
 		chat.emit('chat message', '<p style="width:100%;margin-top:0;margin-bottom:0; padding: 3px; background-color:' + user.backgr + '; color:' + user.font + ';">' + user.username + ' has disconnected</p>');
+		users[id] = null;
 	});
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+server.listen(3000, function(){
+	console.log("- -- -- -- - -- -- -- -- - -- -- - -- -- -- -- - -- -- - -- - ");
+	console.log('echo:> 3000');
 });
 
